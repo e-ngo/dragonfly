530ce0e5967a019a1170901344620718bc208b94
Gaius <gaius.qi@gmail.com>
2025-06-24 12:38:15 +0800

    feat: remove unique index uk_casbin_rule from CasbinRule model to resolve permission issue (#4156)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- manager/models/casbin_rule.go
+++ manager/models/casbin_rule.go
@@ -18,11 +18,11 @@ package models
 
 type CasbinRule struct {
 	ID    uint   `gorm:"primaryKey;autoIncrement;comment:id"`
-	Ptype string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:policy type"`
-	V0    string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:v0"`
-	V1    string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:v1"`
-	V2    string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:v2"`
-	V3    string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:v3"`
-	V4    string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:v4"`
-	V5    string `gorm:"type:varchar(100);default:NULL;uniqueIndex:uk_casbin_rule;comment:v5"`
+	Ptype string `gorm:"type:varchar(100);default:NULL;comment:policy type"`
+	V0    string `gorm:"type:varchar(100);default:NULL;comment:v0"`
+	V1    string `gorm:"type:varchar(100);default:NULL;comment:v1"`
+	V2    string `gorm:"type:varchar(100);default:NULL;comment:v2"`
+	V3    string `gorm:"type:varchar(100);default:NULL;comment:v3"`
+	V4    string `gorm:"type:varchar(100);default:NULL;comment:v4"`
+	V5    string `gorm:"type:varchar(100);default:NULL;comment:v5"`
 }
